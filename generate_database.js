e156965ee39059d2f177eb8b43b527df2937a7e5
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-02-21 14:29:57 +0700

    organize code

--- generate_database.js
+++ generate_database.js
@@ -10,71 +10,77 @@ var MarriageRelation = require('./models/marriage-relation');
 // util
 var hashing = require('./util/hashing');
 
-////////////////////////////////////////////////////////////////////////////////
-// HANDLER FUNCTIONS
-var connect_database_handler = function(err) {
-  if (!!err) {
-	console.log('Unable to connect to the database:', err);
-  } else {
-    console.log('Connection has been established successfully.');
-	create_tables();
-  }
-};
+// execution functions
+var start_generate = function(){
 
-var create_tables_handler = function(err) {
-  if (!!err) {
-	console.log('An error occurred while create the table:', err);
-  } else {
-	console.log('It worked!');
-	insert_user();
-  }
-};
+	// first open the connection to the database
+	sequelize
+		.authenticate()
+		.complete(function(err) {
+			if (!!err) {
+				console.log('Unable to connect to the database:', err);
+			} else {
+				console.log('Connection has been established successfully.');
 
-var insert_user_handler = function(err) {
-  if (!!err) {
-	console.log('The instance has not been saved:', err);
-  } else {
-	console.log('We have a persisted instance now');
-  }
-};
-
-////////////////////////////////////////////////////////////////////////////////
-// EXECUTION FUNCTIONS
-var connect_database = function(){
-  sequelize
-	.authenticate()
-	.complete(connect_database_handler);
+				// create the tables
+				create_tables();
+			}
+		});
 };
 
 var create_tables = function(){
-  sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
-  .then(function(){
-	return sequelize.sync({force: true});
-  })
-  .then(function(){
-	return sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
-  })
-  .then(function(){
-	console.log("Database created");
-	insert_user();
-  }, function(err){
-	console.log('An error occurred while create the table:', err);
-  });
+	// disable foreign key check so that we can delete the existing tables
+	sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
+		.then(function(){
+			return sequelize.sync({force: true});
+		})
+		.then(function(){
+			return sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+		})
+		.then(function(){
+			console.log("Database created");
+
+			// now insert data
+			insert_user();
+			insert_root();
+		}, function(err){
+			console.log('An error occurred while create the table:', err);
+		});
 };
 
 var insert_user = function(){
-  // create a user
-  var user = User.build({
-	username: 'admin',
-	password: hashing.make_hash_pass('password')
-  });
+	// create a user
+	var user = User.build({
+		username: 'admin',
+		password: hashing.make_hash_pass('password')
+	});
 
-  // insert the user
-  user
-	.save()
-	.complete(insert_user_handler);
+	// insert the user
+	user
+		.save()
+		.complete(function(err) {
+			if (!!err) {
+				console.log('The instance has not been saved:', err);
+			} else {
+				console.log('We have a persisted instance now');
+			}
+		});
+};
+
+var insert_root = function(){
+	var husband = Person.build({
+		name: "Root husband"
+	});
+
+	husband.save().complete(function(err){
+		if(err){
+			console.log('Error inserted root husband');
+		} else {
+			console.log('Successfully inserted root husband');
+		}
+	});
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // START
-connect_database();
+start_generate();
