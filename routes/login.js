75753a4c103570e4ed614a23ce224f5fbeaab12a
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-02-21 16:05:03 +0700

    update login form, fix the error message

--- routes/login.js
+++ routes/login.js
@@ -6,14 +6,18 @@ var passport = require('passport');
 
 exports.login_get = function(req, res){
   res.render('login', { user: req.user, message: req.session.messages });
+	req.session.messages = null;
 };
 
 exports.login_post = function(req, res, next) {
+
+	
+	
   passport.authenticate('local', function(err, user, info) {
     if (err)
-	{
-	  return next(err);
-	}
+		{
+			return next(err);
+		}
     if (!user) {
       req.session.messages = [info.message];
       return res.redirect('/login');
