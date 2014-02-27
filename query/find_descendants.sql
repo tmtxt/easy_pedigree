ce41edd513cca3efc6298b612be3ea44ecbb5f3b
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-02-27 22:46:06 +0700

    update query for finding descendant

--- query/find_descendants.sql
+++ query/find_descendants.sql
@@ -1,10 +1,21 @@
-WITH RECURSIVE nodes(insideParentId, outsideParentId, childId) AS (
-	SELECT r1."insideParentId", r1."outsideParentId", r1."childId", p1."name"
-	FROM "PedigreeRelations" AS r1, "People" AS p1
-	WHERE r1."childId" = p1."id" AND r1."insideParentId" = :rootId
-	UNION
-	SELECT r2."insideParentId", r2."outsideParentId", r2."childId", p2."name"
-	FROM "PedigreeRelations" AS r2, "People" AS p2, nodes AS r3
-	WHERE r2."insideParentId" = r3.childId AND r2."childId" = p2."id"
+WITH RECURSIVE nodes(insideParentId, insideParentName, outsideParentId, outsideParentName, childId, childName, childPicture, path) AS (
+	SELECT
+		r."insideParentId", p1."name",
+		r."outsideParentId", p2."name",
+		r."childId", p3."name", p3."picture",
+		ARRAY[r."insideParentId"]
+	FROM "PedigreeRelations" AS r, "People" AS p1, "People" AS p2, "People" AS p3
+	WHERE r."insideParentId" = :rootId
+	AND p1."id" = r."insideParentId" AND p2."id" = r."outsideParentId" AND p3."id" = r."childId"
+	UNION ALL
+	SELECT
+		r."insideParentId", p1."name",
+		r."outsideParentId", p2."name",
+		r."childId", p3."name", p3."picture",
+		path || r."insideParentId"
+	FROM "PedigreeRelations" AS r, "People" AS p1, "People" AS p2, "People" AS p3,
+		nodes AS nd
+	WHERE r."insideParentId" = nd.childId
+	AND p1."id" = r."insideParentId" AND p2."id" = r."outsideParentId" AND p3."id" = r."childId"
 )
 SELECT * FROM nodes;
