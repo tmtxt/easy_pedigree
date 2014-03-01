1c381df8dacc06ebef96af2d92bfd7be8df21653
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-03-01 20:37:19 +0700

    insert member function

--- actions/add-member.js
+++ actions/add-member.js
@@ -1,3 +1,5 @@
+var Person = require('../models/person');
+
 exports.add_member_render = function (req, res){
 	res.render('add-member', {
 		title: "Add member"
@@ -5,5 +7,26 @@ exports.add_member_render = function (req, res){
 };
 
 exports.add_member_insert = function (req, res){
+	console.log(req.body.birthdate);
+	var birthDate = Date.parse(req.body.birthdate);
+	console.log(birthDate);
+	
+	// Person.model.build({
+	// 	name: req.body.name,
+	// 	birthDate: req.body.birthdate,
+	// 	deathDate: req.body.deathdate,
+	// 	isAlive: req.body.isalive,
+	// 	job: req.body.job,
+	// 	address: req.body.address,
+	// 	picture: req.body.picture,
+	// 	gender: req.body.gender,
+	// 	phoneNo: req.body.phoneno,
+	// 	idCard: req.body.idcard,
+	// 	note: req.body.note
+	// }).save().success(function(){
+	// 	console.log('ok');
+	// }).error(function(err){
+	// 	console.log(err);
+	// });
 	
 };
