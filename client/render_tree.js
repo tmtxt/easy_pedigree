d29d5e7fa41748577d4879f5e331cf116d4bba41
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-03-10 12:50:05 +0700

    add some comment

--- client/render_tree.js
+++ client/render_tree.js
@@ -1,26 +1,23 @@
-// some varables
-var
-i = 0,
-root;
-
+// required libraries
 var jquery = require('jquery-browserify');
 var d3 = require('d3-browserify');
 
-var w = jquery("#body").width();
-var h = w;
+var i = 0;
+var root; // this is the hierarchy tree
+var tree, diagonal, vis;        // supporting variables for drawing tree
 
-var tree, diagonal, vis;
+// component size
+var w = jquery("#body").width(); // width
+var h = 1000;                    // height
+var link_height = 150;           // height of the connection link
 
-var link_height = 150;
-
-h = 1000;
-
+// create a tree layout using d3js
 tree = d3.layout.tree()
 	.size([h, w]);
-
 diagonal = d3.svg.diagonal()
 	.projection(function(d) { return [d.x, d.y]; });
 
+// zoom handler
 function zoom() {
   vis.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
@@ -28,6 +25,7 @@ function zoom() {
 // define the zoomListener which calls the zoom function on the "zoom" event constrained within the scaleExtents
 var zoomListener = d3.behavior.zoom().scaleExtent([0.1, 3]).on("zoom", zoom);
 
+// create the svg tag and append to the body of the website
 vis = d3.select("#body").append("svg:svg")
 	.attr("width", w)
 	.attr("height", h)
@@ -35,7 +33,7 @@ vis = d3.select("#body").append("svg:svg")
 	.append("svg:g")
 	.attr("transform", "translate(" + 0 + "," + 80 + ")");
 
-
+// request the tree data from the server and then render
 d3.json("/data/tree-data", function(json) {
 	root = json;
 	root.x0 = w / 2;
@@ -55,8 +53,7 @@ d3.json("/data/tree-data", function(json) {
 	update(root);
 });
 
-
-
+// update for each toggle
 function update(source) {
   var duration = d3.event && d3.event.altKey ? 5000 : 500;
 
@@ -69,14 +66,13 @@ function update(source) {
 	// update the x position
   var offsetLeft = 0;
   var ratio;
-  if(nodes.length == 1){
+  if(nodes.length === 1){
     ratio = root.x / (w/2);
   } else {
     offsetLeft = d3.min(nodes, function(d) {return d.x;});
     offsetLeft -= 50;
     ratio = (root.x - offsetLeft) / (w/2);
   }
-
   nodes.forEach(function(d) {
 		d.x = (d.x - offsetLeft) / ratio;
 	});
@@ -90,11 +86,13 @@ function update(source) {
     .attr("class", "node")
     .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; });
 
+  // the node
   nodeEnter.append("svg:circle")
     .attr("r", 1e-6)
     .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; })
 		.on("click", function(d) { toggle(d); update(d); });
 
+  // text for displaying name
   nodeEnter.append("svg:text")
   //.attr("x", function(d) { return d.children || d._children ? -10 : 10; })
     .attr("y", -19)
@@ -125,14 +123,14 @@ function update(source) {
 	}
 	findMaxDepth(root);
 	var newHeight = (currentMaxDepth + 1) * link_height;
-	// tree = tree.size([newHeight, w]);
 	d3.select("svg").attr("height", newHeight);
 	
   // Transition nodes to their new position.
   var nodeUpdate = node.transition()
     .duration(duration)
     .attr("transform", function(d) { 
-			return "translate(" + d.x + "," + d.y + ")"; });
+			return "translate(" + d.x + "," + d.y + ")";
+    });
 
   nodeUpdate.select("circle")
     .attr("r", 10)
