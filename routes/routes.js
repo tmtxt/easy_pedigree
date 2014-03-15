8b9245e90788a009ca23572ad2d4c82323606ef0
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-03-15 15:39:27 +0700

    add middleware for require auth

--- routes/routes.js
+++ routes/routes.js
@@ -4,32 +4,47 @@ var user = require('../actions/user');
 var tree = require('../actions/tree');
 var add_member = require('../actions/add-member.js');
 
-var middleware = function(req, res, next) {
+function commonMiddleware (req, res, next){
   res.locals.currentUser = req.user;
 	res.locals.title = "Easy Pedigree";
+}
+
+function normalMiddleware(req, res, next) {
+  commonMiddleware(req, res, next);
   next();
-};
+}
+
+function requireAuthMiddleware(req, res, next){
+  commonMiddleware(req, res, next);
+
+  // check if the user is logged in
+  if(!req.isAuthenticated()){
+    res.redirect('/login');
+  }
+  
+  next();
+}
 
 // pass the express app as the input argument
 exports.do_routing = function(app){
 
 	// rendering
-  app.get('/', middleware, homepage.index);
-  app.get('/users', middleware, user.list);
-	app.get('/logout', middleware, authenticate.logout);
-  app.get('/login', middleware, authenticate.login_get);
+  app.get('/', normalMiddleware, homepage.index);
+  app.get('/users', normalMiddleware, user.list);
+	app.get('/logout', normalMiddleware, authenticate.logout);
+  app.get('/login', normalMiddleware, authenticate.login_get);
 
 	// routes for views
-	app.get('/views/tree', middleware, tree.tree_get_render);
-	app.get('/views/add-member', middleware, add_member.add_member_render);
+	app.get('/views/tree', normalMiddleware, tree.tree_get_render);
+	app.get('/views/add-member', requireAuthMiddleware, add_member.add_member_render);
 
 	// routes for getting data
-	app.get('/data/tree-data', middleware, tree.tree_get_data);
-	app.get('/data/tree-max-depth', middleware, tree.tree_get_max_depth);
+	app.get('/data/tree-data', normalMiddleware, tree.tree_get_data);
+	app.get('/data/tree-max-depth', normalMiddleware, tree.tree_get_max_depth);
 
 	// processing request
-  app.post('/login', middleware, authenticate.login_post);
-	app.post('/actions/add-member', middleware, add_member.add_member_insert);
+  app.post('/login', normalMiddleware, authenticate.login_post);
+	app.post('/actions/add-member', normalMiddleware, add_member.add_member_insert);
 };
 
 
