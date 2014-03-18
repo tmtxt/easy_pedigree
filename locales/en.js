90d1d97cdef1eb26b52d0a4e6a3b252047222d5e
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-03-18 21:19:13 +0700

    i18n for authentication

--- locales/en.js
+++ locales/en.js
@@ -4,9 +4,12 @@
   "gnr.homepage": "Homepage",
   "gnr.pedigree_tree": "Pedigree Tree",
   "gnr.add_member": "Add member",
-
   "authen.log_in": "Login",
   "authen.username": "Username",
   "authen.password": "Password",
-  "authen.remember_me": "Remember me"
+  "authen.remember_me": "Remember me",
+  "authen.log_in_error": "Error while logging in",
+  "authen.log_in_success": " Successfully logged in",
+  "authen.log_out_success": "You have logged out",
+  "authen.unknown_user_or_password" : "Either username or password is incorrect"
 }
--- locales/vi.js
+++ locales/vi.js
@@ -4,9 +4,12 @@
   "gnr.homepage": "Trang chủ",
   "gnr.pedigree_tree": "Cây gia phả",
   "gnr.add_member": "Thêm thành viên",
-
   "authen.log_in": "Đăng nhập",
   "authen.username": "Tên đăng nhập",
   "authen.password": "Mật khẩu",
-  "authen.remember_me": "Lưu lại đăng nhập cho lần sau"
+  "authen.remember_me": "Lưu lại đăng nhập cho lần sau",
+  "authen.log_in_error": "Có lỗi xảy ra trong quá trình đăng nhập.",
+  "authen.log_in_success": "Đăng nhập thành công.",
+  "authen.log_out_success": "Đăng xuất thành công.",
+  "authen.unknown_user_or_password" : "Tên đăng nhập hoặc mật khẩu không đúng."
 }
